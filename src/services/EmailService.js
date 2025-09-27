829416ba3bea86017b3e47011a2b1c6f89faa2ee
Gabriel da Fonseca Meira <gabriel.gfm@outlook.com>
2025-09-27 17:42:55 -0300

    feat/ implementação básica do email service

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -1,3 +1,34 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587, 
+    secure: false,
+    auth: {
+        user: "seuemail@provedor.com",
+        pass: ""
+    },
+})
+
 export default class EmailService {
-    //TODO:
+    async sendMail(destinyAdress, subject, template) {
+        let mailOptions = this.#createMailOptions(destinyAdress, subject, template);
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("Erro ao enviar e-mail:", error);
+            } else {
+                console.log("E-mail enviado:", info.response);
+            }
+        });
+    }
+
+    #createMailOptions(destinyAdress, subject, template) {
+        return {
+            from: '"Taskhero" <seuemail@provedor.com>',
+            to: destinyAdress,
+            subject: subject,
+            html: template
+        }
+    }
 }
